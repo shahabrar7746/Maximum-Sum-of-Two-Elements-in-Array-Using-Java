90257dafb7d4b0ba8d0f58ff00fd060b43d316e6
shahabrar7746 <73398955+shahabrar7746@users.noreply.github.com>
2021-05-09 14:03:18 +0530

    all issue solved

--- MaximumSumOfElementsInArray.java
+++ MaximumSumOfElementsInArray.java
@@ -13,36 +13,34 @@ public class MaximumSumOfElementsInArray {
 		for(int i = 0;i<array.length;i++) {
 			array[i] = s.nextInt();
 		}
+		
 		//max variable will contain element with maximum value
-               //This will find max for calulation of final answer and secondary maximum value
+boolean verify = true;
+//This will loop varify if there is any repetitive values is there
+		for(int i = 0;i<array.length;i++) {
+			for(int j = i+1;j<array.length;j++) {
+				if(array[i] == array[j]) {
+					verify = false;
+				}
+			}
+		}
+		if(verify) {
 int max = 0;
 int s_min = 0;
-for(int i = 0;i<array.length;i++) {
-	if(max < array[i]) {
-		
-		max = array[i];
-	}
-}
-//This loop will put max value at the tale of array to calculate secondary max
 int temp = 0;
-for(int i = 0;i<array.length;i++) {
-	if(max == array[i]) {
-		temp = array[array.length - 1];
-		array[array.length - 1] = max;
-		array[i] = temp;
-	}
-}
-max = 0;
-//This loop will calculate max ans secondary max value simultaneously 
-for(int i = 0;i<array.length;i++) {
-	if(max < array[i]) {
-		s_min = max;
-		max = array[i];
-		break;
-	}
-}
+//This method will be used to sort array in ascending order using array class			
+Arrays.sort(array);
+
+max = array[array.length - 1];
+s_min = array[array.length - 2];
+
+
 System.out.println("Maximum Sum of Elements in Array is " + max + s_min);
 System.out.println("And the Element Giving Maximum Sum is " + max + " " + s_min);
+	}else {
+		System.out.println("Array Contains Repeatetive values Hence Maximum sum cannot be calculated");
 	}
-
+	}
 }
+
+	
